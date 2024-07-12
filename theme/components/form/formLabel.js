ccd9800c611f7b1b5e08ad37bfb711bca8dc2f49
Nazir Ali Siddiqui <120049478+nazirali007@users.noreply.github.com>
2024-07-12 17:46:06 +0530

    Add files via upload

---
+++ theme/components/form/formLabel.js
@@ -0,0 +1,29 @@
+/**
+=========================================================
+* Gurash Dahboard MUI - v3.0.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-material-ui
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// Gurash Dahboard MUI base styles
+import colors from "assets/theme/base/colors";
+
+const { dark } = colors;
+
+const formLabel = {
+  styleOverrides: {
+    root: {
+      color: dark.main,
+    },
+  },
+};
+
+export default formLabel;
